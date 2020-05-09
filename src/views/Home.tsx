ea4104b082ccf2ac11451260efed473b8bd9bc20
Devon Miller-Junk <devonmillerjunk@gmail.com>
2020-05-09 16:44:07 -0400

    Introduce Routing

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,22 +1,22 @@
 import React, { Component } from "react";
-import Contact from "./containers/Contact";
-// import Experience from "./containers/Experience";
-import Interest from "./containers/Interest";
-import Profile from "./containers/Profile";
-// import Project from "./containers/Project";
+import Contact from "../containers/Contact";
+// import Experience from "../containers/Experience";
+import Interest from "../containers/Interest";
+import Profile from "../containers/Profile";
+// import Project from "../containers/Project";
 import { Carousel } from "react-bootstrap";
 
-import background from "./assets/background.jpg";
-import background1 from "./assets/background1.jpg";
-import background2 from "./assets/background2.jpg";
-import background3 from "./assets/background3.jpg";
-import background4 from "./assets/background4.jpg";
+import background from "../assets/background.jpg";
+import background1 from "../assets/background1.jpg";
+import background2 from "../assets/background2.jpg";
+import background3 from "../assets/background3.jpg";
+import background4 from "../assets/background4.jpg";
 
-interface ICarouselState {
+interface IHomeState {
   backgroundImages: any[];
 }
 
-class App extends Component<any, ICarouselState> {
+class Home extends Component<any, IHomeState> {
   constructor(props: any) {
     super(props);
 
@@ -68,4 +68,4 @@ class App extends Component<any, ICarouselState> {
   };
 }
 
-export default App;
+export default Home;
