878b5d4ae33163f380b03c1ac052b96600b06d8c
DevonMillerJunk <devonmillerjunk@gmail.com>
2021-06-01 21:58:02 -0400

    delete old files, start addition of new file

--- src/App.tsx
+++ src/App.tsx
@@ -6,24 +6,22 @@ import {
   Redirect,
 } from "react-router-dom";
 import Home from "./views/Home";
-import Doorr from "./views/Doorr";
-import LCBO from "./views/LCBO";
-import Finastra from "./views/Finastra";
 import { RoutesEnum } from "./constants/interfaces";
 
 class App extends Component<any, any> {
   public render() {
     return (
       <div className="App">
+        <div className="app-border-container"/>
+        <div className="app-container scroll" >
         <Router>
+          <div className="app-container-spacer"/>
           <Switch>
             <Route exact path={RoutesEnum.HOME} component={Home} />
-            <Route exact path={RoutesEnum.DOORR} component={Doorr} />
-            <Route exact path={RoutesEnum.LCBONEXT} component={LCBO} />
-            <Route exact path={RoutesEnum.FINASTRA} component={Finastra} />
             <Route render={() => <Redirect to={RoutesEnum.HOME} />} />
           </Switch>
         </Router>
+        </div>
       </div>
     );
   }
--- src/containers/Project.tsx
+++
@@ -1,5 +0,0 @@
-import React, { Component } from "react";
-import { RouteComponentProps } from "react-router";
-
-class Project extends Component<any, any> {}
-export default Project;
